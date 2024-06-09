cfdcceed0a6d61815fae6214b57ca69a8175fd8c
Tawfeeq111 <mohammedtawfeeq945@gmail.com>
2024-06-09 18:48:48 +0530

    Cloudinary and Multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+//configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+//uploading on cloudinary
+const uploadOnCoudinary = async (localPath) => {
+    try{
+
+        if(!localPath) return null
+
+        const uploadResult = await cloudinary.uploader.upload(localPath, { resource_type: "auto" })
+
+        console.log("File is uploaded on cloudinary : ", uploadResult.url)
+
+        return uploadResult
+    } 
+    catch(error){
+        console.log("error in uploading file in couldinary", error)
+        fs.unlinkSync(localPath)
+        return null
+    }
+}
+
+export default uploadOnCoudinary
